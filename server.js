9ee8d73d0fac1ae6ef323822f474662d9c34ed5f
picasso <46543347+djreichel@users.noreply.github.com>
2019-04-24 12:03:24 -0400

    another update

--- server.js
+++ server.js
@@ -21,14 +21,17 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static files
+app.use(express.static('app/public'));
+
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
