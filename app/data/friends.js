efd66bf1aa928555401137dddac9b9b935a2ef1c
picasso <46543347+djreichel@users.noreply.github.com>
2019-04-24 10:49:22 -0400

    upload all

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [{
+        "name": "Manny",
+        "photo": "http://1.bp.blogspot.com/-prbMeqdMCS0/T_5mVhMScVI/AAAAAAAANog/jJeRZubdjuQ/s1600/iceage-cast01.png",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        "name": "Sid",
+        "photo": "https://i.pinimg.com/736x/3b/3c/b5/3b3cb5e1acd0e157b3c9e417ccd837b4.jpg",
+        "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        "name": "Diego",
+        "photo": "https://i.pinimg.com/originals/61/1c/ae/611cae00645fcb90ada0a5ea41f0b072.jpg",
+        "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        "name": "Crash & Eddie",
+        "photo": "https://i.pinimg.com/originals/97/66/bf/9766bfa1623a6f135288155111b101ce.jpg",
+        "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        "name": "Ellie",
+        "photo": "http://ffe-ugc.s3.amazonaws.com/ice-age/production/character/headshot_image/6/char_headshot_ellie.jpg",
+        "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    }
+]
+
+module.exports = friends;
